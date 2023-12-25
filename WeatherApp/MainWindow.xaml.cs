ae1d88e681b788c36476cc252deeee874d1c6329
rushaan1 <cocrushaan3@gmail.com>
2023-12-25 12:39:01 +0530

    Added information with UI elements

--- WeatherApp/MainWindow.xaml.cs
+++ WeatherApp/MainWindow.xaml.cs
@@ -27,8 +27,14 @@ namespace WeatherApp
     {
         private GeoCoordinateWatcher watcher;
         private bool setWeatherData = false;
+        private char tempUnit = 'C';
+        private int minTemp;
+        private int maxTemp;
+        private int currentTemp;
+        private int feelsLike;
         public MainWindow()
         {
+            Trace.WriteLine("wus good");
             InitializeComponent();
             watcher = new GeoCoordinateWatcher(GeoPositionAccuracy.Default);
             if (!setWeatherData)
@@ -58,6 +64,65 @@ namespace WeatherApp
             }
         }
 
+        int convert(int celsius) 
+        {
+            int value = celsius;
+            if (tempUnit == 'F') 
+            {
+                value = ToF(celsius);
+            }
+            return value;
+        }
+
+        int ToC(int f) 
+        {
+            double c = (f - 32) * 5 / 9;
+            return Convert.ToInt32(c);
+        }
+
+        int ToF(int c)
+        {
+            double f = (c * 9.0) / 5.0 + 32.0;
+            return Convert.ToInt32(f); 
+        }
+
+        private void Celsius(object sender, RoutedEventArgs e)
+        {
+            if (tempUnit == 'F')
+            {
+                currentTemp = ToC(currentTemp);
+                feelsLike = ToC(feelsLike);
+                minTemp = ToC(minTemp);
+                maxTemp = ToC(maxTemp);
+
+                temperature.Content = currentTemp + "°C";
+                feelslike.Content = "Feels Like " + feelsLike + "°C";
+                min.Content = "Min " + minTemp + "°C";
+                max.Content = "Max " + maxTemp + "°C";
+            }
+
+            tempUnit = 'C';
+            Canvas.SetLeft(snowOverlay, 358);
+        }
+
+
+        private void Faren(object sender, RoutedEventArgs e)
+        {
+            if (tempUnit == 'C')
+            {
+                temperature.Content = ToF(currentTemp) + "°F";
+                currentTemp = ToF(currentTemp);
+                feelslike.Content = "Feels Like " + ToF(feelsLike) + "°F";
+                feelsLike = ToF(feelsLike);
+                min.Content = "Min " + ToF(minTemp) + "°F";
+                minTemp = ToF(minTemp);
+                max.Content = "Max " + ToF(maxTemp) + "°F";
+                maxTemp = ToF(maxTemp);
+            }
+            tempUnit = 'F';
+            Canvas.SetLeft(snowOverlay, 411);
+        }
+
         private void SettingWeatherData(object sender, GeoPositionChangedEventArgs<GeoCoordinate> e)
         {
             // This event is triggered when the location is changed
@@ -69,6 +134,7 @@ namespace WeatherApp
             string Response = Get(GetOpenWeatherUrl(latitude, longitude, "metric"));
             Trace.WriteLine(Response);
             JsonTextReader reader = new JsonTextReader(new StringReader(Response));
+            bool mainSet = false;
             while (reader.Read())
             {
                 if (reader.Value != null)
@@ -78,11 +144,66 @@ namespace WeatherApp
                         reader.Read();
                         double temp = (double)reader.Value;
                         int tempInt = Convert.ToInt32(temp);
-                        this.Dispatcher.Invoke(() => { temperature.Content = tempInt + "°C"; });
-                        setWeatherData = true;
+                        currentTemp = convert(tempInt);
+                        this.Dispatcher.Invoke(() => { temperature.Content = convert(tempInt) + "°C"; });
+                    }
+                    if (reader.Value.Equals("main") && !mainSet) 
+                    {
+                        reader.Read();
+                        weather.Content = (string)reader.Value;
+                        mainSet = true;
+                    }
+                    if (reader.Value.Equals("feels_like")) 
+                    {
+                        reader.Read();
+                        double fl = (double)reader.Value;
+                        this.feelsLike = convert(Convert.ToInt32(fl));
+                        feelslike.Content = "Feels Like "+convert(Convert.ToInt32(fl)) + "°C";
+                    }
+                    if (reader.Value.Equals("temp_min"))
+                    {
+                        reader.Read();
+                        double mint = (double)reader.Value;
+                        this.minTemp = convert(Convert.ToInt32(mint)); 
+                        min.Content = "Min " + convert(Convert.ToInt32(mint)) + "°C"; 
+                    }
+                    if (reader.Value.Equals("temp_max"))
+                    {
+                        reader.Read();
+                        double maxt = (double)reader.Value;
+                        this.maxTemp = convert(Convert.ToInt32(maxt));
+                        max.Content = "Max " + convert(Convert.ToInt32(maxt)) + "°C";
+                    }
+
+
+                    if (reader.Value.Equals("pressure")) 
+                    {
+                        reader.Read();
+                        Trace.WriteLine(reader.Value);
+                        //int v = (int)reader.Value;
+                        presssure.Content = "Air Pressure: " + reader.Value;
+                    }
+                    if (reader.Value.Equals("humidity"))
+                    {
+                        reader.Read();
+                        //int v = (int)reader.Value;
+                        humidity.Content = "Humidity: " + reader.Value +"%";
+                    }
+                    if (reader.Value.Equals("visibility"))
+                    {
+                        reader.Read();
+                        //int v = (int)reader.Value;
+                        visibility.Content = "Visibility: " + reader.Value + "m";
+                    }
+                    if (reader.Value.Equals("speed"))
+                    {
+                        reader.Read();
+                        //double v = (double)reader.Value;
+                        wspeed.Content = "Wind Speed: " + reader.Value +"m/s";
                     }
                 }
             }
+            this.setWeatherData = true;
         }
     }
 }
